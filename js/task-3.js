6f30fcd5d3493deb3834857e185d45e3995791cf
Olena Matsak <161434803+matsak-olena-p94@users.noreply.github.com>
2024-05-17 14:53:35 +0300

    finish

--- js/task-3.js
+++ js/task-3.js
@@ -2,8 +2,8 @@ function getElementWidth(content, padding, border) {
     content = Number(content)
     padding = Number(padding)
     border = Number(border)
-    return content + (padding * 2) + (border * 2); 
-    return = Number(return)
+    const borderBox = content + (padding * 2) + (border * 2); 
+    return = Number(borderBox)
 }
 
 console.log(getElementWidth("50px", "8px", "4px"));
